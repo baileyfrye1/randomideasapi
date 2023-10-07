773f72c6cc30ff7af280cd7518e27dbf376bb604
Bailey Frye <baileyfrye@Baileys-Mac-mini.local>
2023-10-07 13:17:10 -0500

    Finished app, made file ready for production

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -15,14 +15,34 @@ class IdeaList {
         this._validTags.add('inventions');
     }
 
+    addEventListeners() {
+        this._ideaListEl.addEventListener('click', (e) => {
+            if(e.target.classList.contains('fa-times')) {
+                e.stopImmediatePropagation();
+                const ideaId = e.target.parentElement.parentElement.dataset.id;
+                this.deleteIdea(ideaId);
+            }
+        })
+    }
+
     async getIdeas() {
         try {
             const res = await IdeasApi.getIdeas();
             this._ideas = res.data.data;
             this.render();
-            console.log(this._ideas);
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    async deleteIdea(ideaId) {
+        try {
+            // Delete from server
+            const res = await IdeasApi.deleteIdea(ideaId);
+            this._ideas.filter((idea) => idea._id !== ideaId);
+            this.getIdeas();
+        } catch (error) {
+            alert('You can not delete this resource');
         }
     }
 
@@ -45,15 +65,17 @@ class IdeaList {
     render() {
         this._ideaListEl.innerHTML = this._ideas.map((idea) => {
             const tagClass = this.getTagClass(idea.tag);
+            const deleteBtn = idea.username === localStorage.getItem('username') ? `<button class="delete"><i class="fas fa-times"></i></button>` : '';
             return `
-            <div class="card">
-            <button class="delete"><i class="fas fa-times"></i></button>
+            <div class="card" data-id="${idea._id}">
+            ${deleteBtn}
             <h3>${idea.text}</h3>
             <p class="tag ${tagClass}">${idea.tag.toUpperCase()}</p>
             <p>Posted on <span class="date">${idea.date}</span> by <span class="author">${idea.username}</span></p>
           </div>
             `;
         }).join('');
+        this.addEventListeners();
     }
 }
 
